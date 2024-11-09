7030cdec4d6aa45dc743836f43337433e3483c17
Scott Fleming <sdflem@gmail.com>
2024-11-09 16:50:37 -0600

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root to: 'home#show'
+
+  # Devise routes for user sign-in, sign-up, sign-out, etc.
+  devise_for :users
+end
